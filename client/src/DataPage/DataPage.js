9de85c83e7a83b3c7db3648e9b0edad0acc83191
sandy8111112004 <mingshiuantsai1992@gmail.com>
2019-01-29 22:23:57 -0500

    delete buttom style

--- client/src/DataPage/DataPage.js
+++ client/src/DataPage/DataPage.js
@@ -6,22 +6,53 @@ import Grid from 'react-css-grid';
 import PieChart from 'react-minimal-pie-chart';
 import * as $ from 'axios';
 
+const Legend = (props) =>(
+    <div>
+        {props.title}: {props.value} <span><div className='color-box' style={props.color}></div></span>
+    </div>
+)
+
 
 const DrawPieChart =(props)=>(
     <div>
-        {props.data.length!==0?<PieChart
+        <div className='question-style'>
+        {props.selection[props.index].question}
+        </div>
+    <div className='chart-box'>
+        <div id='pie-box' className='box'>
+        <PieChart
             data={props.data}
             radius={20}
-        />:null}
+        />
+        </div>
+        <div className='legend-box box'>
+            {props.data.map((e,i)=><Legend key={i} title={e.title} value={e.value} color={{'backgroundColor':e.color}}/>)}
+        </div>
+    </div>
     </div>
 )
 
 const ResultDataList =(props)=>(
     <div>
-        {props.content}
+        <div className='question-style'>{props.selection[props.index].question}</div>
+        <div className='answer-style'>Total: {props.content} </div>
     </div>
-
 )
+
+const  ResultBox = (props) => (
+    <div>
+        <div>
+            <div className='question-style'>
+        Total Submit Survey Number: {props.total}
+        </div>
+        {props.dataCal?props.dataCal.map((e,i)=>e.length===1?<ResultDataList selection={props.selection} index={i} content={e[0]} key={i}/>:null):null}
+        </div>
+        <Grid width='40vw' gap={0} >
+        {props.dataCal?props.dataCal.map((e,i)=>e.length===1?null:<DrawPieChart selection={props.selection} index={i} data={e.slice(1,e.length)} key={i} colorArr={props.colorArr}/>):null}
+        </Grid>
+    </div>
+)
+
 
 const DetailedSurvey = (props) => (
     <div>
@@ -74,7 +105,7 @@ const SelectionEntry = (props) => (
 const DataList = (props) => (
     <div>
         <li className='dataEntry' onClick={() => props.handleDisplay(props.index)}> {props.entry[0][0].answer} 
-        <span><button onClick={()=>props.answerDeleteHandler(props.index)}>Delete</button></span></li>
+        <span>  <button id='deleteListBtn' onClick={()=>props.answerDeleteHandler(props.index)}  class="fas fa-trash-alt"></button></span></li>
         
     </div>
 )
@@ -89,10 +120,8 @@ const PageContens = (props) => (
 
         <div className='chart-box'>
 
-
-        {props.dataCal?props.dataCal.map((e,i)=>e.length===1?<ResultDataList content={e[0]} key={i}/>:<DrawPieChart data={e.slice(1,e.length)} key={i} colorArr={props.colorArr}/>):'wait'}
-     
-       
+        <ResultBox dataCal={props.dataCal} selection={props.selection} total={props.total}/>
+          
         
         </div>
     </div>
@@ -110,28 +139,10 @@ class DataPage extends React.Component {
         pageID:'',
         pageTitle:'',
         dataCal:[],
-        colorArr:['#E38627', '#C13C37','#6A2135','#35A5A5', 'FB0A27','0AFB14']
+        colorArr:['#E38627', '#C13C37','#6A2135','#35A5A5', '#FB0A27','#0AFB14','#210AFB','#29FB0A']
     }
 
-    // attendingRate = () => {
-    //     //e.preventDefault();
-    //     let result = 0;
-    //     let answerarr = this.state.surveyData.map(resultArr => resultArr.filter(data => data[0].id === '5c43f9d2f15727502cad2244'));
-    //     answerarr = answerarr.filter(e => e[0][0].answer === 'Yes');
-    //     result = answerarr.length / this.state.surveyData.length;
-    //     result = parseFloat(result.toFixed(2));
-    //     console.log('Attending Rate: ', result);
-    //     this.setState({ attendingRate: result });
-    // }
-
-    // chartData = () =>{
-    //     const answerData =this.state.surveyData;
-    //     const chartNum=answerData[0].filter(data=>data[0].id);
-    //     for(let i=0;i<chartNum.length;i++){
-    //         let filteredArr=answerData.map(resultArr=>resultArr.filter(data=>data[0].id===chartNum[i]));
-    //         isNaN(filteredArr[0])?this.ynAnswer() :this.numAnswer() ;
-    //     }
-    // }
+   
 
     dataAnalysis=()=>{
         //e.preventDefault();
@@ -151,7 +162,7 @@ class DataPage extends React.Component {
                         let index=numArr[i].findIndex(e=>e.title===drawingData[j][lenQue+i][0].answer);
                         numArr[i][index].value += 1;
                     }else{
-                        numArr[i].push({title: drawingData[j][lenQue+i][0].answer, value: 1, color:this.state.colorArr[(j+i)%5]});
+                        numArr[i].push({title: drawingData[j][lenQue+i][0].answer, value: 1, color:this.state.colorArr[(j+i)%(this.state.colorArr.length)]});
                     }
                 }
             }
@@ -172,7 +183,7 @@ class DataPage extends React.Component {
         const id = url.substring(index + 5);
         $.get(`/api/survey/${id}`)
             .then((result) => {
-                //console.log("hit the first cb")
+
                 this.setState({
                     surveyData: result.data.answer,
                     selection: result.data.selection,
@@ -180,8 +191,7 @@ class DataPage extends React.Component {
                     pageID: result.data._id,
                     pageTitle: result.data.title
                 }, function(){
-                   // console.log("hit the second cb")
-                   // this.attendingRate();
+
                     this.dataAnalysis();
                 });
 
@@ -222,6 +232,8 @@ class DataPage extends React.Component {
                         pageTitle={this.state.pageTitle}
                         checkData = {this.dataAnalysis}
                         colorArr={this.state.colorArr}
+                        selection={this.state.selection}
+                        total={this.state.surveyData.length}
                     />
                 </div>
                 <div id='raw-data-display'>
